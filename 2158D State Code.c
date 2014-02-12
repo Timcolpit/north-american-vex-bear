6eb214e17011c6dd3208a1a6ebe101ad6f5179c4
Timcolpit <timothy.colpitts@gmail.com>
2014-02-12 13:34:40 -0600

    Commented out variables with problems
    I commented out the  void turnDriverR and L variable so that they can be
    fixed.

--- 2158D State Code.c
+++ 2158D State Code.c
@@ -90,7 +90,51 @@ void moveIntake(int direction) {
 		motor[takeLeft]	   = FULL_POWER * direction;
 		motor[takeRight]   = FULL_POWER * direction;
 	}
+	/*
+		void turnDriveR(int direction, int timer=0) {
+	// Check for function input error
+	if(direction == 1 || direction == -1 || direction == 0){
+		// Right Turn
+		motor[frontRightMotor]	 = FULL_POWER * direction;
+		motor[frontLeftMotor] 	 = HALF_POWER * direction;
+		motor[backRightMotor]	   = FULL_POWER * direction;
+		motor[backLeftMotor]	   = HALF_POWER * direction;
+	}
+
+	if(timer != 0) {
+		// Wait ?timer? ms
+		wait1Msec(timer);
+
+		// Stop
+		motor[frontRightMotor]	 = STOP;
+		motor[frontLeftMotor] 	 = STOP;
+		motor[backRightMotor]	   = STOP;
+		motor[backLeftMotor]	   = STOP;
+	}
+	void turnDriveL(int direction, int timer=0) {
+	// Check for function input error
+	if(direction == 1 || direction == -1 || direction == 0){
+		// Move at 100% power
+		motor[frontRightMotor]	 = HALF_POWER * direction;
+		motor[frontLeftMotor] 	 = FULL_POWER * direction;
+		motor[backRightMotor]	   = HALF_POWER * direction;
+		motor[backLeftMotor]	   = FULL_POWER * direction;
+	}
+
+	if(timer != 0) {
+		// Wait ?timer? ms
+		wait1Msec(timer);
+
+		// Stop
+		motor[frontRightMotor]	 = STOP;
+		motor[frontLeftMotor] 	 = STOP;
+		motor[backRightMotor]	   = STOP;
+		motor[backLeftMotor]	   = STOP;
+	}
 }
+*/
+}
+
 
 //Wait for Press--------------------------------------------------
 void waitForPress()
@@ -119,7 +163,10 @@ void waitForRelease()
 
 void pre_auton()
 {
-		//Display the Primary Robot battery voltage
+	clearLCDLine(0);
+	clearLCDLine(1);
+
+	//Display the Primary Robot battery voltage
 		displayLCDString(0, 0, "Primary: ");
 		sprintf(mainBattery, "%1.2f%c", nImmediateBatteryLevel/1000.0, 'V'); //Build the value to be displayed
 		displayNextLCDString(mainBattery);
